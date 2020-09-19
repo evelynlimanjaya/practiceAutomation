79461358054c5fc82ec4f11525420f2a731d1117
evelynlimanjaya <crimson.eve@gmail.com>
2020-09-18 20:53:14 -0700

    Page Object practice for nightwatch automation test

---
+++ pageObjects/weatherman.js
@@ -0,0 +1,12 @@
+module.exports={
+    url: 'https://devmountain-qa.github.io/weatherman/build/index.html',
+    elements: {
+        searchBar:'.enter-location__input',
+        resultCity:'.current-weather__location',
+        errorMessage:'.error-message__message',
+        searchButton:'.enter-location__submit',
+        searchAgainButton:'.current-weather__search-again',
+        tryAgainButton:'.error-message__try-again'
+
+    }
+}
---
+++ pageObjects/yoodlizePageObject.js
@@ -0,0 +1,125 @@
+module.exports = {
+    url: 'https://alpha.yoodlize.com/',
+    elements: {
+        searchCategory: {
+            selector: '//div[@class="sc-jKVCRD jSqgxr"]',
+            locateStrategy: 'xpath'
+        },
+        recVehicleSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[1]',
+            locateStrategy: 'xpath'
+        },
+        outGearSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[2]',
+            locateStrategy: 'xpath'
+        },
+        electronicsSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[3]',
+            locateStrategy: 'xpath'
+        },
+        partySeeAll: {
+            selector: '(//div[contains(text(),"See all")])[4]',
+            locateStrategy: 'xpath'
+        },
+        toolsSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[5]',
+            locateStrategy: 'xpath'
+        },
+        clothingSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[6]',
+            locateStrategy: 'xpath'
+        },
+        homeSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[7]',
+            locateStrategy: 'xpath'
+        },
+        toysSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[8]',
+            locateStrategy: 'xpath'
+        },
+        lawnSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[9]',
+            locateStrategy: 'xpath'
+        },
+        sportSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[10]',
+            locateStrategy: 'xpath'
+        },
+        dvdSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[11]',
+            locateStrategy: 'xpath'
+        },
+        venuesSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[12]',
+            locateStrategy: 'xpath'
+        },
+        musicSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[13]',
+            locateStrategy: 'xpath'
+        },
+        businessSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[14]',
+            locateStrategy: 'xpath'
+        },
+        miscSeeAll: {
+            selector: '(//div[contains(text(),"See all")])[15]',
+            locateStrategy: 'xpath'
+        },
+        toolsButton: {
+            selector: '(//div[@class="f-center-start h-45"])[1]',
+            locateStrategy: 'xpath'
+        },
+        outdoorButton: {
+            selector: '(//div[@class="f-center-start h-45"])[2]',
+            locateStrategy: 'xpath'
+        },
+        electronicsButton: {
+            selector: '(//div[@class="f-center-start h-45"])[3]',
+            locateStrategy: 'xpath'
+        },
+        partyButton: {
+            selector: '(//div[@class="f-center-start h-45"])[4]',
+            locateStrategy: 'xpath'
+        },
+        recVehiclesButton: {
+            selector: '(//div[@class="f-center-start h-45"])[5]',
+            locateStrategy: 'xpath'
+        },
+        clothingButton: {
+            selector: '(//div[@class="f-center-start h-45"])[6]',
+            locateStrategy: 'xpath'
+        },
+        homeButton: {
+            selector: '(//div[@class="f-center-start h-45"])[7]',
+            locateStrategy: 'xpath'
+        },
+        toysButton: {
+            selector: '(//div[@class="f-center-start h-45"])[8]',
+            locateStrategy: 'xpath'
+        },
+        lawnButton: {
+            selector: '(//div[@class="f-center-start h-45"])[9]',
+            locateStrategy: 'xpath'
+        },
+        sportButton: {
+            selector: '(//div[@class="f-center-start h-45"])[10]',
+            locateStrategy: 'xpath'
+        },
+        dvdButton: {
+            selector: '(//div[@class="f-center-start h-45"])[11]',
+            locateStrategy: 'xpath'
+        },
+        venuesButton: {
+            selector: '(//div[@class="f-center-start h-45"])[12]',
+            locateStrategy: 'xpath'
+        },
+        musicButton: {
+            selector: '(//div[@class="f-center-start h-45"])[13]',
+            locateStrategy: 'xpath'
+        },
+        miscButton: {
+            selector: '(//div[@class="f-center-start h-45"])[14]',
+            locateStrategy: 'xpath'
+        },
+    }
+}
