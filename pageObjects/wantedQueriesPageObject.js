25e57b352a1c3b6b7b9345fbbb303394fdef7db7
evelynlimanjaya <crimson.eve@gmail.com>
2020-09-21 16:09:38 -0700

    DevMountain week 3 project Page Object for practicing function

---
+++ pageObjects/wantedQueriesPageObject.js
@@ -0,0 +1,13 @@
+module.exports={
+    url:'http://localhost:3000/#/',
+    elements:{
+        menuButton: {
+        selector:'//*[@class="bm-burger-button"]',
+        locateStrategy:'xpath'},
+        enterOption: '[name="enterOption"]',
+        overlay:'[class="bm-overlay"]',
+        saveButton:'#saveBtn',
+        
+
+    }
+}
