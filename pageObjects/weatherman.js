595a0614976d65daeeaf3ede602b52e1a73b09c7
evelynlimanjaya <crimson.eve@gmail.com>
2020-10-10 09:27:06 -0700

    Practice javascript automation

--- pageObjects/weatherman.js
+++ pageObjects/weatherman.js
@@ -1,5 +1,17 @@
+var weatherCommands={
+    search: function (city){
+        this
+            .setValue('@searchBar', [city,this.api.Keys.ENTER])
+            .waitForElementPresent('@resultCity')
+            .expect.element('@resultCity').text.to.equal(city)
+        this.click('.current-weather__search-again')
+        return this
+    }
+}
+
 module.exports={
     url: 'https://devmountain-qa.github.io/weatherman/build/index.html',
+    commands:[weatherCommands],
     elements: {
         searchBar:'.enter-location__input',
         resultCity:'.current-weather__location',
