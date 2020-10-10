064a1174b8ae921f17613f11ad98f75ace37621e
evelynlimanjaya <crimson.eve@gmail.com>
2020-10-10 09:28:38 -0700

    Practice javascript automation

--- tests/google.js
+++ tests/google.js
@@ -6,11 +6,29 @@ module.exports = {
         browser.end()
     },
     'Search test': browser => {
+        var result='#res'
         browser
             .setValue('input[name="q"]',['odin sphere',browser.Keys.ENTER])
             .waitForElementVisible('#res')
-            .pause(5000)
-            .verify.containsText('#res','Odin Sphere')
+            .pause(2000)
+            .getText('#res', check=>{
+                var searchCheck=check.value.search(/yakiniku|odin sphere/i)
+                console.log(searchCheck)
+                if(searchCheck!==-1){
+                    console.log('Search match')
+                }
+                else{
+                    console.log("Search doesn't match")
+                }
+            })
     }
 
 }
+
+// var string='Andrew is a very cute boy'
+// var result=string.split(' ')
+// var finalResult=String(result)
+// var forRegExp=finalResult.replace(/,/g,'|')
+// console.log(forRegExp)
+// var resultRegExp=RegExp(forRegExp,'i')
+// console.log(resultRegExp)
