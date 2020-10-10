f2f71a859ae9f41a292283e16be12fcedc5fb2ce
evelynlimanjaya <crimson.eve@gmail.com>
2020-10-10 09:30:54 -0700

    Practice javascript automation

--- tests/weatherTests.js
+++ tests/weatherTests.js
@@ -1,47 +1,58 @@
-var weatherPage
+var weatherPage = {}
+
+var searchData=[
+    "San Diego",
+    "San Jose",
+    "Los Angeles"]
+
 module.exports = {
     beforeEach: browser => {
         weatherPage = browser.page.weatherman()
         weatherPage.navigate()
     },
-    'Search for city': browser => {
-        weatherPage
-            .setValue('@searchBar', ['San Diego', browser.Keys.ENTER])
-            .waitForElementPresent('@resultCity')
-            .expect.element('@resultCity').text.to.equal('San Diego')
+    after: browser=>{
+        weatherPage.end()
     },
-    'Search for zip': browser => {
-        weatherPage
-            .setValue('@searchBar', ['95820', browser.Keys.ENTER])
-            .waitForElementPresent('@resultCity')
-            .expect.element('@resultCity').text.to.equal('Sacramento')
-    },
-    'Search for blank': browser => {
-        weatherPage
-            .setValue('@searchBar', ['', browser.Keys.ENTER])
-            .waitForElementPresent('@errorMessage')
-            .expect.element('@errorMessage').text.to.equal('There was a problem fetching the weather!')
-    },
-    'Search for bad zip': browser => {
-        weatherPage
-            .setValue('@searchBar', ['123456789', browser.Keys.ENTER])
-            .waitForElementPresent('@errorMessage')
-            .expect.element('@errorMessage').text.to.equal('There was a problem fetching the weather!')
-    },
-    'Search again': browser => {
-        weatherPage
-            .setValue('@searchBar', '95820')
-            .click('@searchButton')
-            .waitForElementPresent('@resultCity')
-            .click('@searchAgainButton')
-            .expect.element('@searchBar').to.be.visible.before(5000)
-    },
-    'Try again': browser => {
-        weatherPage
-            .setValue('@searchBar', '')
-            .click('@searchButton')
-            .waitForElementPresent('@errorMessage')
-            .click('@tryAgainButton')
-            .expect.element('@searchBar').to.be.visible.before(5000)
+    // 'Search for city': browser => {
+    //     searchData.forEach(test=>{searchFunction(weatherPage,test.input)})
+    // },
+
+    'Custom command':browser=>{
+            weatherPage
+                .search(searchData[2])
     }
+    // 'Search for zip': browser => {
+    //     weatherPage
+    //         .setValue('@searchBar', ['95820', browser.Keys.ENTER])
+    //         .waitForElementPresent('@resultCity')
+    //         .expect.element('@resultCity').text.to.equal('Sacramento')
+    // },
+    // 'Search for blank': browser => {
+    //     weatherPage
+    //         .setValue('@searchBar', ['', browser.Keys.ENTER])
+    //         .waitForElementPresent('@errorMessage')
+    //         .expect.element('@errorMessage').text.to.equal('There was a problem fetching the weather!')
+    // },
+    // 'Search for bad zip': browser => {
+    //     weatherPage
+    //         .setValue('@searchBar', ['123456789', browser.Keys.ENTER])
+    //         .waitForElementPresent('@errorMessage')
+    //         .expect.element('@errorMessage').text.to.equal('There was a problem fetching the weather!')
+    // },
+    // 'Search again': browser => {
+    //     weatherPage
+    //         .setValue('@searchBar', '95820')
+    //         .click('@searchButton')
+    //         .waitForElementPresent('@resultCity')
+    //         .click('@searchAgainButton')
+    //         .expect.element('@searchBar').to.be.visible.before(5000)
+    // },
+    // 'Try again': browser => {
+    //     weatherPage
+    //         .setValue('@searchBar', '')
+    //         .click('@searchButton')
+    //         .waitForElementPresent('@errorMessage')
+    //         .click('@tryAgainButton')
+    //         .expect.element('@searchBar').to.be.visible.before(5000)
+    // }
 }
